46b9ae2298742d232045180320015f3b1c7400c7
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-01-24 23:37:01 +0800

    It's a new telegram bot

---
+++ bot.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+require 'telegram/bot'
+require 'ghee'
+require 'time'
+require 'pp'
+
+require_relative 'module'
+
+TOKEN = "298625375:AAG51PT_LTCGbsZdiAqdFIJcPRbtPyNZ1xw"
+gh = Ghee.basic_auth("???", "???")
+updatetime = Hash.new
+Unicorn::get_time(updatetime, gh)
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+    receive_thread = Thread.new do
+        bot.listen do |message|
+            begin
+                case message.text
+                when '/start'
+                    Unicorn::store_chatid(message.chat.id)
+                    bot.api.send_message(chat_id: message.chat.id, text: "Hello,welcome to use @ujnblogbot.")
+                when '/stop'
+                    # todo
+                    bot.api.send_message(chat_id: message.chat.id, text: "See you next time.")
+                end
+            rescue
+                next
+            end
+        end
+    end
+
+    send_thread = Thread.new do
+        while true
+            sleep 120
+            begin
+                str = Unicorn::check(updatetime, gh)
+                if (str.size != 0)
+                    Unicorn::user_list.each do |user|
+                        bot.api.send_message(chat_id: user.to_i, text: "#{str}")
+                    end
+                    Unicorn::get_time(updatetime, gh)
+                end
+            rescue
+                next
+            end
+        end
+    end
+
+    receive_thread.join
+    send_thread.join
+end
---
+++ module.rb
@@ -0,0 +1,66 @@
+# encoding: utf-8
+
+module Unicorn
+    version = "1.0"
+
+    def get_time(updatetime, authentication)
+        File.open("allblog.ini", "r") do |io|
+            while line = io.gets
+                line.chomp!
+                updatetime[line] = Time.parse(pushed_at(line, authentication).to_s.gsub!(/T/, " ").gsub!(/Z/, ""))
+            end
+        end
+    end
+
+    def pushed_at(user, authentication)
+        return authentication.repos(user, "#{user}.github.io").to_h['pushed_at']
+    end
+
+    def check(updatetime, authentication)
+        newupdatetime = Hash.new
+        str = String.new
+
+        get_time(newupdatetime, authentication)
+        return str if(updatetime == newupdatetime)
+
+        updatetime.each do |key, value|
+            if newupdatetime[key] > value
+                str << "#{key} update Blogs #{key}.github.io"
+            end
+        end
+
+        return str if str.size != 0
+    end
+
+    def user_list
+        user = Array.new
+        File.open("chat_id.ini", "r") do |io|
+            while line = io.gets
+                line.chomp!
+                user << line
+            end
+        end
+        return user
+    end
+
+    def store_chatid(chat_id)
+        chat_id = chat_id.to_s
+        flagbit = true
+        user_list.each do |user|
+            next if user != chat_id
+            flagbit = false if user == chat_id
+        end
+
+        if (flagbit)
+            io_temp = File.open("chat_id.ini", "a")
+            io_temp << chat_id << "\n"
+            io_temp.close
+        end
+    end
+
+    module_function :get_time
+    module_function :pushed_at
+    module_function :check
+    module_function :user_list
+    module_function :store_chatid
+end
